49bb60924c5c3e4d7a76357a949210748cd11dea
Shavin <64917885+Singhmansingh@users.noreply.github.com>
2022-05-20 22:10:21 -0400

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,40 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <GameScreen />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import GameScreen from './components/GameScreen.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    GameScreen
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="sass">
+@font-face 
+  font-family: "Qarmic Sans"
+  src: url('./assets/fonts/Qarmic sans Abridged.ttf')
+
+
+#app 
+  font-family: "Qarmic Sans"
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale
+  text-align: center
+  color: #2c3e50
+  box-sizing: border-box
+  background: #f4f4f4
+  width: 100%
+  height: 100%
+  padding: 20px
+  display: flex
+
+body 
+  margin: 0
+  height: 100vh
+  width: 100vw
+
 </style>
